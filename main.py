7083e9f02c200ebe3f885726b64e2985eff4ece1
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-04-03 11:52:07 +0200

    dodano: endpoint do uczenia nowych pytań i odpowiedzi

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from flask_restful import Api
 
 from rest_api.api_endpoint_hello import ProjectWelcomeMessage
 from rest_api.api_endpoint_learn_spam import ChatbotLearnSpamData
+from rest_api.api_endpoint_learn_question import ChatbotLearnQuestionData
 
 
 app = Flask(__name__)
@@ -10,6 +11,7 @@ api = Api(app)
 
 api.add_resource(ProjectWelcomeMessage, '/')
 api.add_resource(ChatbotLearnSpamData, '/learn/spam')
+api.add_resource(ChatbotLearnQuestionData, '/learn/question')
 
 if __name__ == '__main__':
     app.run()
