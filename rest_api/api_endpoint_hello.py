7d2ae6299ba35316186b12ba041b716f3393cc2c
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-04-03 11:30:38 +0200

    dodano: informacje o autorach projektu

--- rest_api/api_endpoint_hello.py
+++ rest_api/api_endpoint_hello.py
@@ -4,5 +4,13 @@ from flask_restful import Resource
 class ProjectWelcomeMessage(Resource):
     def get(self):
         return {
-            'hello': 'world'
+            'przedmiot': 'Projekt programistyczny',
+            'kierunek': 'Informatyka',
+            'grupa': 'Studia niestacjonarne',
+            'rok': '2021/2022',
+            'uczelnia': 'Uniwersytet Ekonomiczny',
+            'autorzy': [
+                'Artur Leśnik',
+                'Alicja Pęgiel'
+            ]
         }
