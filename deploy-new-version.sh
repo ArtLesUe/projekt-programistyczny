d5ce3bd5025929daee17b1b953e63ec5ded5bb4e
Artur Leśnik <artur.lesnik@edu.uekat.pl>
2022-05-20 18:54:12 +0200

    dodano: automatyczne śledzenie konsoli po zbudowaniu

--- deploy-new-version.sh
+++ deploy-new-version.sh
@@ -4,3 +4,4 @@ docker stop chatbot-uekat
 docker image rm chatbot-uekat-gr2-inf-nies
 docker build -t chatbot-uekat-gr2-inf-nies ./
 docker run -d --name chatbot-uekat --rm -p 5000:5000 chatbot-uekat-gr2-inf-nies
+docker logs -f chatbot-uekat
